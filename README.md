5558b0e36763614bfe2d786480751aac116bb3fd
Alex Cherednichenko <alex.cherednichenko@c-change-labs.com>
2023-08-04 13:22:54 -0400

    Initial code

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# python-openepd-sample
+# EC3 Public API Python client example
+
+## References
+
+* Public API (openEPD) for EC3: https://openepd.buildingtransparency.org/#/epds/post_epds
+* Documentation on authentication: https://staging.buildingtransparency.org/ec3/manage-apps/api-doc/guide#/02_Accessing_API/01_Authentication.md
+
+## Installing
+
+* Python 3.10 or higher
+* Optional: use virtual environment to isolate python packages
+  * install venv, and create a virtual environment with `python -m venv venv`
+  * switch to it by running `source venv/bin/activate`
+* Python packages: requests. Install with `pip install requests`
+
+## Running
+
+* Create an account by registered at [EC3 Staging](https://staging.buildingtransparency.org)
+* Contact Apisupport@buildingtransparency.org to get access to EPD Creators permissions
+* Create an API key via [Key management section](https://staging.buildingtransparency.org/ec3/manage-apps/keys) with 
+  `READ WRITE` scopes. Store the `token` somewhere locally.
+* Run `TOKEN=replace_token python ./ec3_public_api_sample.py`, replacing `replace_token` with the token you got from 
+  the previous step.
+
+Script would output results of the operation.
+Please see json files for object examples.
---
+++ ec3_public_api_sample.py
@@ -0,0 +1,67 @@
+import json
+import os
+from pprint import pprint
+
+import requests
+
+SEPARATOR = "_" * 40
+
+
+def main():
+    auth_token = os.getenv("TOKEN")
+    base_url = "http://localhost:8000/openepd"
+    # base_url = "https://openepd.staging.buildingtransparency.org/api"
+    headers = {"Authorization": f"Bearer {auth_token}"}
+
+    created_epd_id = None
+
+    print_section("Creating necessary organisations")
+    # ensure we have verifier, manufacturer, and program_operator, the hard way
+    for org_json in ["manufacturer.json", "program_operator.json", "third_party_verifier.json"]:
+        with open(f"{org_json}") as f:
+            org = json.load(f)
+
+            response = requests.post(f"{base_url}/orgs", json=org, headers=headers)
+            if response.status_code == 201:
+                print(f"Created org by {org_json}")
+            else:
+                print(f"Exists? Org creation failed with {response.status_code}. {response.text}")
+
+    # ensure we have a plant, the hard way
+    print_section("Creating dependency plant")
+    with open("plant.json") as f:
+        plant = json.load(f)
+        response = requests.post(f"{base_url}/plants", json=plant, headers=headers)
+        if response.status_code == 201:
+            print("Created new plant by plant.json")
+        else:
+            print(f"Exists? Plant creation failed with {response.status_code}. {response.text}")
+
+    # deposit an EPD
+    print_section("Depositing an EPD")
+    with open("epd.json") as f:
+        epd = json.load(f)
+        response = requests.post(f"{base_url}/epds", json=epd, headers=headers)
+        if response.status_code == 201:
+            created_epd_id = response.json()['id']
+            print(f"Created new EPD, id: {created_epd_id}")
+        else:
+            print(f"EPD creation failed with {response.status_code}. {response.text}")
+
+    # now, read it back form API
+    print_section("Reading back created EPD from the API")
+    if created_epd_id:
+        response = requests.get(f"{base_url}/epds/{created_epd_id}", headers=headers)
+        if response.status_code == 200:
+            print(f"Read back EPD: {pprint(response.json())}")
+        else:
+            print(f"EPD read failed with {response.status_code}. {response.text}")
+
+
+def print_section(section_name: str):
+    print(SEPARATOR)
+    print(f"> {section_name}\n")
+
+
+if __name__ == "__main__":
+    main()
